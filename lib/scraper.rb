aed993234b5bd2f48695d21f7a9b9b7fe5928cc9
imperative-genre-4923 <dreama.lin@yale.edu>
2020-07-06 20:37:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
 
     doc.css("div.vitals-container div.social-icon-container a").each do |e|
       if e.attr("href").include?("twitter")
-        student_profile[:tiwtter] = e.attr("href")
+        student_profile[:twitter] = e.attr("href")
       elsif e.attr("href").include?("linkedin")
         student_profile[:linkedin] = e.attr("href")
       elsif e.attr("href").include?("github")
