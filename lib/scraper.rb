6dbfb5bd33ed67182131eff9f56c2ce3998daa4c
imperative-genre-4923 <dreama.lin@yale.edu>
2020-07-06 19:24:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,14 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    doc.css("div.student-card").collect do |student|
+      {:name = studnet.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.attr("href")
+      }
+    end
+
 
   end
 
